0faf44a1910ba8e1b2d2c235587af9049b2cfeef
Allan182 <allan.furlani@hotmail.com>
2023-06-07 19:50:50 -0300

    Add Page About and create Perfil for user.

--- src/paginas/About/index.js
+++ src/paginas/About/index.js
@@ -7,13 +7,28 @@ function About() {
 
     <div className={styles.about}>
 
-      <h1 className={styles.error}> About </h1>
+      <h1 className={styles.title}> We Help You Ever </h1>
 
-      <p className={styles.paragraph}> The prestigious King School is an exemplary institution that sets the bar high for excellence in education. From its state-of-the-art facilities to its dedicated faculty, this school provides an unparalleled learning environment for students. The curriculum at Harmony Academy is meticulously designed to foster both academic and personal growth, ensuring that students receive a well-rounded education. With a diverse range of extracurricular activities and clubs, students have the opportunity to explore their passions and develop their talents. </p>
+      <p className={styles.subtitle}>
+        Experiment with us and we'll give you a reason to trust and study with us.
+      </p>
 
+      <div className={styles.container}>
+
+
+        <p className={styles.paragraph}>
+          <h1> King School </h1>
+
+          The prestigious King School is an exemplary institution that sets the bar high for excellence in education.
+          From its state-of-the-art facilities to its dedicated faculty, this school provides an unparalleled learning environment for students.We are waiting for you, come meet us!
+
+        </p>
+
+        <div className={styles.containerImage}>  </div>
+      </div>
     </div>
 
-)
+  )
 
 
 }
--- src/paginas/AlunoPost/index.js
+++ src/paginas/AlunoPost/index.js
@@ -18,14 +18,26 @@ function AlunoPost() {
   return (
     <>
       <div className={styles.AlunoPost}>
-        <div className={styles.AlunoDescricao}>
 
-          <h1>  {aluno.nome} </h1>
-          <p> Idade: {aluno.idade} </p>
-          <p> Slug: {aluno.slug} </p>
-          <p> ID: {aluno.id} </p>
-          <p> Disciplinas: {aluno.disciplinas.map(e => <p> Nome: {e.nome} | Professor: {e.professor} </p>)}</p>
+        <h1 className={styles.title}> Perfil </h1>
+        <p className={styles.subtitle}> I'm a back-end web developer </p>
 
+        <div className={styles.container}>
+
+          <div className={styles.sobreMim}>
+            <p> <h1>  Sobre mim </h1>   I am an allround web developer. I am a senior programmer with good knowledge of front-end techniques. Vitae sapien pellentesque habitant morbi tristique senectus et. Aenean sed adipiscing diam donec adipiscing tristique risus.
+            </p>
+          </div>
+
+          <div className={styles.AlunoDescricao}>
+            <h1> Descrição </h1>
+            <p>  Nome: {aluno.nome} </p>
+            <p> Idade: {aluno.idade} </p>
+            <p> Slug: {aluno.slug} </p>
+            <p> ID: {aluno.id} </p>
+            <p> Disciplinas: {aluno.disciplinas.map(e => <p> Nome: {e.nome} | Professor: {e.professor} </p>)}</p>
+
+          </div>
         </div>
       </div>
     </>
